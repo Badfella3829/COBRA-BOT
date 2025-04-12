af15eb92d992112049a1f7da3a74ef0c0838d2e0
BADFELLA CHAHAL <ku9690845@gmail.com>
2025-04-12 18:10:30 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -4,8 +4,8 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22801364"))
-API_HASH = environ.get("API_HASH", "38fd13fcb81f612aa703fa63d7dffec8")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-OWNER = int(os.environ.get("OWNER", "1996039956"))
-ADMINS = (os.environ.get("ADMINS", "1996039956").split())
+API_ID = int(environ.get("API_ID", "27942404"))
+API_HASH = environ.get("API_HASH", "27aec80c9bf45e866e5b7b33cb39fb0f")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8148114386:AAF6sZmNqNA5EETDGcg9k2YYPXkdK1gG0Z8")
+OWNER = int(os.environ.get("OWNER", "7534723821"))
+ADMINS = (os.environ.get("ADMINS", "7534723821").split())
